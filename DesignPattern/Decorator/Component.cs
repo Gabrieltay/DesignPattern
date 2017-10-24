29238e4bcfdfb32a63ad166a478ef87f3d210fda
Gabrieltay <Gabrieltaykl@gmail.com>
2017-10-25 00:51:11 +0800

    Init

---
+++ DesignPattern/Decorator/Component.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Decorator
+{
+    public interface IComponent
+    {
+        string Operation();
+    }
+
+    public class Component : IComponent
+    {
+        public string Operation()
+        {
+            return "Normal Operation";
+        }
+    }
+
+    public class StrongComponent : IComponent
+    {
+        IComponent mComponent;
+
+        public StrongComponent( IComponent aComponent )
+        {
+            mComponent = aComponent;
+        }
+
+        public string Operation()
+        {
+            return String.Format( "{0} + STRONG", mComponent.Operation() );
+        }
+    }
+
+    public class LightComponent : IComponent
+    {
+        IComponent mComponent;
+
+        public LightComponent( IComponent aComponent )
+        {
+            mComponent = aComponent;
+        }
+
+        public string Operation()
+        {
+            return String.Format( "{0} + LIGHT", mComponent.Operation() );
+        }
+    }
+
+    public class ExtraComponent : IComponent
+    {
+        IComponent mComponent;
+
+        public ExtraComponent( IComponent aComponent)
+        {
+            mComponent = aComponent;
+        }
+
+        public string Operation()
+        {
+            return String.Format( "{0} + EXTRA", mComponent.Operation() );
+        }
+
+        public string NewOperation()
+        {
+            return String.Format( "[{0}]", this.Operation() );
+        }
+    }
+}
---
+++ DesignPatternTest/DecoratorTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPattern.Decorator;
+
+namespace DesignPatternTest
+{
+    class DecoratorTest
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "Hello World" );
+        }
+    }
+}
